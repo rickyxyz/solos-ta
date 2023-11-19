758a36b867e930e730cab707bd4820a6f91bd5eb
rickyxyz <56119701+rickyxyz@users.noreply.github.com>
2023-11-19 15:14:17 +0700

    feat: remove query param input

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,10 @@
 import { fetchPlayers } from '@/lib/actions/player.actions';
-import { PlayerFilter, playerFilter } from '@/types/player';
 import GridPlayer from '@/components/GridPlayer';
 
-interface HomePageProps {
-  searchParams: { filter: string };
-}
+async function HomePage() {
+  const playerData = await fetchPlayers();
 
-async function HomePage({ searchParams }: HomePageProps) {
-  const filter = playerFilter.includes(searchParams.filter as PlayerFilter)
-    ? (searchParams.filter as PlayerFilter)
-    : 'all position';
-
-  const playerData = await fetchPlayers(filter);
-
-  return <GridPlayer playerData={playerData} filterParam={filter} />;
+  return <GridPlayer playerData={playerData} filterParam={'all position'} />;
 }
 
 export default HomePage;
